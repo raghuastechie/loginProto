e9eb32eab584cdeb6b0822af9c2b53b9f6e0ed62
raghuastechie <raghuastechie@gmail.com>
2025-01-02 17:00:11 +0530

    completed index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const express = require('express');
+const app = express();
+require('dotenv').config();
+const port = process.env.PORT;
+console.log(port);
+
+app.get('/', (req, res) => {
+    res.status(200).send({msg: 'success'});
+});
+
+app.listen(port, () => console.log(`Listening On Port: ${port}`));
